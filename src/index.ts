2a430c6b1d521bf5e2dc93d4480bb5d84253ca1e
xDivisionByZerox <leyla.jaehnig@gmx.de>
2023-09-04 00:56:55 +0200

    feat: add base program

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-// todo write the application
+import { Command } from 'commander';
+import { description, version } from '../package.json';
+
+const program = new Command()
+  .name('faker')
+  .version(version)
+  .description(description)
+  .usage('faker [options] module_name entry_name');
+
+const commands: Command[] = [];
+for (const command of commands) {
+  // implement sub commands
+}
+
+program.parse(process.argv);
+
+const hasArgs = process.argv.length > 2;
+if (!hasArgs) {
+  program.help();
+}
