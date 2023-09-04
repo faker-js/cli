0b82a935c2d22df3eca5aa631d522c9f63e38d5e
xDivisionByZerox <leyla.jaehnig@gmx.de>
2023-09-04 18:27:27 +0200

    feat: add simply binary file

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,21 @@ import { Command } from 'commander';
 import { description, version } from '../package.json';
 import commands from './commands';
 
-const program = new Command()
-  .name('faker')
-  .version(version)
-  .description(description)
-  .usage('faker [options] module_name entry_name');
+export function cli(args: string[]) {
+  const program = new Command()
+    .name('faker')
+    .version(version)
+    .description(description)
+    .usage('faker [options] module_name entry_name');
 
-for (const command of commands) {
-  program.addCommand(command);
-}
+  for (const command of commands) {
+    program.addCommand(command);
+  }
 
-program.parse(process.argv);
+  program.parse(args);
 
-const hasArgs = process.argv.length > 2;
-if (!hasArgs) {
-  program.help();
+  const hasArgs = args.length > 2;
+  if (!hasArgs) {
+    program.help();
+  }
 }
