f2fd555fb1ea155d4d447983404b4ac9e03e34d7
xDivisionByZerox <leyla.jaehnig@gmx.de>
2025-12-28 23:46:26 +0100

    refactor: autogenerate API entrypoint for easier command building

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,8 @@
 import { Command } from 'commander';
 import { description, version } from '../package.json';
+import { API } from './api';
 import { ArgumentError } from './errors/argument.error';
 import { ReferenceError } from './errors/reference.error';
-
-function getValueByKey(obj: object, key: string): unknown {
-  return Object.entries(obj)
-    .find(([objectKey]) => objectKey === key)
-    ?.at(1);
-}
 
 export function cli(args: string[]) {
   const program = new Command()
@@ -16,35 +11,32 @@ export function cli(args: string[]) {
     .description(description)
     .argument('<moduleName>', 'The name of the module to invoke.')
     .argument('<functionName>', 'The name of the function to invoke.')
-    .action(async (moduleName, functionName) => {
+    .action((moduleName, functionName) => {
       if (typeof moduleName !== 'string') {
         throw new ArgumentError('moduleName', moduleName);
+      }
+
+      const moduleRef = API[moduleName];
+      if (moduleRef === undefined) {
+        throw new ReferenceError('module', moduleName);
       }
 
       if (typeof functionName !== 'string') {
         throw new ArgumentError('functionName', functionName);
       }
 
-      const { faker } = await import('@faker-js/faker/locale/en');
-      const moduleRef = getValueByKey(faker, moduleName);
-      if (typeof moduleRef !== 'object' || moduleRef === null) {
-        throw new ReferenceError('module', moduleName);
+      const entryRef = moduleRef[functionName];
+      if (entryRef === undefined) {
+        throw new ReferenceError('function', moduleName);
       }
 
-      const entry = getValueByKey(moduleRef, functionName);
-      if (typeof entry !== 'function') {
-        throw new ReferenceError('function', functionName);
-      }
-
-      // TODO: Need to figure out how to strictly type this function call
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      console.log(entry());
+      console.log(entryRef());
     });
-
-  program.parse(args);
 
   const hasArgs = args.length > 2;
   if (!hasArgs) {
     program.help();
+  } else {
+    program.parse(args);
   }
 }
